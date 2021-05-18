e1132bf54232535f6127530daeac201963d2752e
Pedro Leta <pedrogleta@gmail.com>
2021-05-18 15:20:50 -0300

    finish home layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,30 @@
-import styles from '../styles/home.module.scss'
+import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton'
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.title}>
-      Teste
-      <span>oi</span>
-    </h1>
+    <>
+      <Head>
+        <title>Home | IgNews</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome!</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 monthly</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
   )
 }
